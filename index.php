517edbe6d7a6bd68da422e0c66af6e14d3a75f72
Remco Kroonen <99039459@mydavinci.nl>
2017-02-09 13:43:17 +0100

    eerste commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="goede.css">
+	<meta charset="utf-8">
+	<title>Goede</title>
+</head>
+<body>
+	<?php
+	date_default_timezone_set("Europe/Amsterdam");
+	//echo date_default_timezone_get("Europe/Amsterdam");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	?>
+
+
+</body>
+</html>

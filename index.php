24f425309f29375c346db1d2872992583b87c09b
Remco Kroonen <99039459@mydavinci.nl>
2017-03-15 11:58:18 +0100

    bezig met plaatjes in te laden.

--- index.php
+++ index.php
@@ -8,23 +8,30 @@
 	<?php
 	date_default_timezone_set('Europe/Amsterdam');
 
-	$time = date("H: i: s");
+	$time = date("H: i");
+	
 	//$datum = ("")
-	echo "$time";
 	if ($time < 12) {
 		# code...
+		echo "$time";
 		echo "goede morgen";
+		echo '<link rel="image" type="text/css" href="img/morning.png"></head>';
+		
+		
 	}
 	elseif (12 < 18) {
 		# code...
+		echo "$time";
 		echo "goede middag";
 	}
 	elseif (18 < 0) {
 		# code...
+		echo "$time";
 		echo "goede avond";
 	}
 	elseif (0 < 6) {
 		# code...
+		echo "$time";
 		echo "goede nacht";
 	}
 

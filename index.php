92e3c68aff9af8d71d01cb49bad5a9f14c08769e
Remco Kroonen <99039459@mydavinci.nl>
2017-03-28 14:14:30 +0200

    text staat in het midden.

--- index.php
+++ index.php
@@ -3,46 +3,41 @@
 <head>
 	<meta charset="utf-8">
 	<title>Goede</title>
+	<link rel="stylesheet" type="text/css" href="goede.css">
 </head>
 <body>
-	<?php
-	date_default_timezone_set('Europe/Amsterdam');
+		<?php
+		date_default_timezone_set('Europe/Amsterdam');
 
-	$time = date("H:i");
-	
+		$time = date("H:i");
+	//los css bestand
 	//$datum = ("")
-	if ($time < 6) {
+		if ($time < 6) {
 		# code...
-		echo "Het is nu: $time <br> goede nacht!";
-		//echo "goede nacht!";
-		echo "<style>body{background-image: url('img/night.png');}</style>";
-	}
-	elseif ($time < 12) {
+			echo  "Het is nu: $time";
+			echo "goede nacht!";
+			echo "<style>body{background-image: url('img/night.png');}</style>";
+		}
+		elseif ($time < 12) {
 		# code...
-		echo "Het is nu: $time <br> goede morgen!";
-		//echo "goede morgen!";
-		echo "<style>body{background-image: url('img/morning.png');}</style>";
-		
-		
-	}
-	elseif ($time < 18) {
+			echo "Het is nu: $time";
+			echo "goede morgen!";
+			echo "<style>body{background-image: url('img/morning.png');}</style>";
+
+
+		}
+		elseif ($time < 18) {
 		# code...
-		echo "Het is nu: $time <br> goede middag!";
-		echo "<style>body{background-image: url('img/afternoon.png');}</style>";
-		//echo "goede middag!";
-	}
-	else {
+			echo "<h1>goede middag!</h1><p>Het is nu: $time</p>";
+			echo "<style>body{background-image: url('img/afternoon.png');}</style>";
+			//echo "";
+		}
+		else {
 		# code...
-		echo "Het is nu: $time <br> goede avond!";
-		echo "<style>body{background-image: url('img/evening.png');}</style>";
-		//echo "goede avond!";
-	}
-
-
-
-
-	?>
-
-
+			echo "Het is nu: $time";
+			echo "<style>body{background-image: url('img/evening.png');}</style>";
+			echo "goede avond!";
+		}
+		?>
 </body>
 </html>
